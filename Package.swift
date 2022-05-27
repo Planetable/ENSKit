1d4273fc21834bab1fd47b21aed1f2147ed04d6f
stdc105 <1missgirlphobia@gmail.com>
2022-05-27 11:51:21 -0600

    upgrade CryptoSwift
    - prevent a build error that `BigUInt` are both in CryptoSwift and BigInt

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/attaswift/BigInt.git", from: "5.3.0"),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.4.3"),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.5.1"),
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.1"),
         .package(url: "https://github.com/stdc105/UInt256.git", from: "0.3.0"),
     ],
